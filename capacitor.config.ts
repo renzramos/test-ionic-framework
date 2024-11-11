2305d61eddce4e5dabbd0bc423ea2fc75e3c2f38
Renz Ramos <im.renzramos@gmail.com>
2024-11-11 16:04:49 +0800

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'test.ionic.ic.framework',
+  appName: 'test-ionic-ic-framework',
+  webDir: 'www'
+};
+
+export default config;
